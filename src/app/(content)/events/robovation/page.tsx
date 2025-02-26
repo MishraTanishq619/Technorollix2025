48147dd3517e8a389cf3a49e50d10b0f1cf40075
Prish0708 <prishiswithu@gmail.com>
2025-02-26 23:46:38 +0530

    updated masterchef , robovation , rodies time and venue

--- src/app/(content)/events/robovation/page.tsx
+++ src/app/(content)/events/robovation/page.tsx
@@ -8,12 +8,13 @@ const page = () => {
   return (
     <div>
       <EventIntro
+      venue={" BAPUJI CHOWK (OPJU)"}
+      time={" "}
         imageUrl="/antaragni.png"
         title="Robovation"
         registrations={346}
         pricepool={30000}
-        description="OPJU invites you to an extraordinary culinary battleground, where flavors meet creativity and passion transforms into perfection. Step into MasterChef and let the world savor your signature taste!."
-      />
+        description="OPJU invites you to an extraordinary culinary battleground, where flavors meet creativity and passion transforms into perfection. Step into MasterChef and let the world savor your signature taste!." time={''} venue={''}      />
        <div className="mt-16 mb-16"></div>
 <h1 className="text-5xl text-[#ffad3c] font-medium text-center mb-14 [text-shadow:_0px_4px_10px_rgb(0_0_0_/_0.78)]">
           SUB EVENTS
