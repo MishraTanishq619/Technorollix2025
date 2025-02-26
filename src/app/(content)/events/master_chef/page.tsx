47fa71574b812e735feb5cb0be6a78d8ee2ddc5b
Prish0708 <prishiswithu@gmail.com>
2025-02-26 22:05:30 +0530

    masterchef updated

--- src/app/(content)/events/master_chef/page.tsx
+++ src/app/(content)/events/master_chef/page.tsx
@@ -1,6 +1,7 @@
 import ButtonLanding from "@/components/sub-component/button-landing";
 import EventConclusion from "@/components/sub-component/event-conclusion";
 import EventIntro from "@/components/sub-component/event-intro";
+import EventManagers from "@/components/sub-component/event-managers";
 import ManagerCard from "@/components/sub-component/manager-card";
 import React from "react";
 
@@ -9,41 +10,38 @@ const page = () => {
     <div className="mb-10">
       <EventIntro
         imageUrl="/antaragni.png"
-        title="Master-Chef"
+        title="MasterChef"
         registrations={346}
         pricepool={30000}
-        description="Unleash your inner chef at our university's ultimate MasterChef event! Showcase your culinary skills, experiment with flavors, and compete in exciting challenges. Whether you're a kitchen novice or a seasoned cook, join us for a thrilling experience of creativity, taste, and teamwork!"
+        description="OPJU invites you to an extraordinary culinary battleground, where flavors meet creativity and passion transforms into perfection. Step into MasterChef and let the world savor your signature taste!."
       />
       <div className="mt-16 mb-16">
         <h1 className="text-5xl text-[#ffad3c] font-medium text-center mb-14 [text-shadow:_0px_4px_10px_rgb(0_0_0_/_0.78)]">
           THEME
         </h1>
         <p className="text-2xl font-['Inter'] leading-[29.08px] tracking-[6.72px] text-white text-center max-w-4xl mx-auto">
-          A recipe has no soul; you as the cook must bring soul to the recipe.
+          Every dish is a chapter, every bite a sentence, and every flavor a
+          memory waiting to be told
         </p>
       </div>
 
       <section className="mb-20">
         <h2 className="text-5xl text-[#ffad3c] font-medium text-center mb-14 [text-shadow:_0px_4px_10px_rgb(0_0_0_/_0.78)]">
-          WHY PARTICIPATE
+          COMPITATION FORMAT
         </h2>
         <div className="bg-[#33010140] p-6 rounded-lg shadow-lg max-w-5xl mx-auto">
           <ul className="list-disc pl-5 text-2xl space-y-2 uppercase font-['Inter'] leading-[29.08px] tracking-[6.72px] text-white">
-            <li>Exciting prizes worth ₹</li>
             <li>
-              Opportunity for funding innovative culinary creations (as per OPJU
-              Innovation Centre terms)
+              Prizes:🏆 First, Second, and Third Place Winners, Three
+              Consolation Prizes
             </li>
-            <li>Recognition certificates for all participating universities</li>
-            <li>Participation certificates for all participants</li>
-            <li>Special category-wise winning prizes</li>
+            <li>• Total Participants: 100 students</li>
+            <li>• Teams: 25 teams (4 students per team)</li>
+            <li>• Time & Number of Rounds: To be intimated later</li>
+            <li>• Registration Fee: ₹100 per group.</li>
             <li>
-              Hands-on experience in creativity, presentation, and culinary
-              mastery
-            </li>
-            <li>
-              Reimbursement of one-way sleeper class fare for outside
-              participants
+              • Eligibility: UG & PG students (Age: 18 - Below 30) from any
+              college/university.
             </li>
           </ul>
         </div>
@@ -55,27 +53,47 @@ const page = () => {
         </h2>
         <div className="bg-[#33010140] p-6 rounded-lg shadow-lg max-w-5xl mx-auto">
           <ul className="list-disc pl-5 text-2xl space-y-2 uppercase font-['Inter'] leading-[29.08px] tracking-[6.72px] text-white">
-            <li>Two chances are given in case of battery or part failure.</li>
-            <li>Maximum battery capacity: 3 4 A cell (2200 3000 mah)</li>
-            <li>Drone Weight: 2KG.</li>
-            <li>Drone size: 4 to 8 inches.</li>
-            <li>Factory made drones are not allowed</li>
-            <li>Participants must arrive 30 minutes before the event starts.</li>
+            <li>1. Teams must register in groups of 4 members.</li>
+            <li>2. This is open to other Institutions outside OPJU</li>
+            <li>3. Each team must distribute tasks among members</li>
+            <li>
+              4. Abusive, offensive, politically driven, gender-biased, or
+              personal remarks will be dealt strictly.{" "}
+            </li>
+            <li>5. Costumes should be suitable for the event.</li>
+            <li>6. Registration is mandatory </li>
+            <li>7. No outside ingredients are allowed</li>
+            <li>8. Teams must finish within the given time.</li>
+            <li>9. Points will be deducted for messy stations.</li>
+            <li>
+              10. Proper handling of knives, stoves, and other kitchen equipment
+              is mandatory.
+            </li>
+            <li>
+              11. Judging Criteria:
+              <li>o Taste 40%</li>
+              <li>o Creativity 30%</li>
+              <li>o Presentation 20%</li>
+              <li>o Cleanliness & Organization 10%</li>
+            </li>
+            <li>12. The judges' decision is final.</li>
+            <li>13. Winners will be declared on the same day.</li>
           </ul>
         </div>
       </section>
 
       <div className="flex flex-col items-center justify-center">
-        <div className="w-[577.70px] h-[85px] text-center text-[#ffad3c] text-5xl font-medium font-['Poppins'] uppercase tracking-[2.88px] [text-shadow:_0px_4px_10px_rgb(0_0_0_/_0.78)]">
+        {/* <div className="w-[577.70px] h-[85px] text-center text-[#ffad3c] text-5xl font-medium font-['Poppins'] uppercase tracking-[2.88px] [text-shadow:_0px_4px_10px_rgb(0_0_0_/_0.78)]">
           EVENT MANAGERS
         </div>
         <div className="w-[1023px] text-center text-white text-2xl font-normal font-['Inter'] tracking-[5.28px]">
           For any queries regarding the event, feel free to reach out to any of
           the managers listed below. They are here to help and ensure a smooth
           experience for you!
-        </div>
+        </div> */}
 
         {/* <ManagerCard /> */}
+        <EventManagers />
 
         <ButtonLanding label="Register Now" />
       </div>
