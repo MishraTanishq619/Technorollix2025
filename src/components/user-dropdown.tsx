f99e2f963f492be5c22bfc84502b525574ecfea2
MishraTanishq619 <mishratanishq619@gmail.com>
2025-02-28 10:32:21 +0530

    user-dropdown-fix

--- src/components/user-dropdown.tsx
+++ src/components/user-dropdown.tsx
@@ -1,95 +1,113 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useRouter } from "next/navigation";
 import { logout } from "@/lib/utils"; // Assuming you have a logout utility function
+import useFetch from "@/hooks/use-fetch";
+import { getUser } from "@/actions/user-actions";
+import { toast } from "@/hooks/use-toast";
 
 const UserDropdown = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  // Example state to track user authentication
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+	// Example state to track user authentication
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // Check if the user is authenticated (you can replace this with your actual auth logic)
-  useEffect(() => {
-    const user = localStorage.getItem("token"); // Replace this with actual auth state logic
-    setIsAuthenticated(!!user); // Set true if user exists
-  }, []);
+	// Check if the user is authenticated (you can replace this with your actual auth logic)
 
-  // Handle logout
-  const handleLogout = () => {
-    logout(); // Add your logout functionality here
-    setIsAuthenticated(false); // Update the state after logout
-    router.push("/"); // Redirect after logout
-  };
+	const {
+		data: userFetchedData,
+		error: userFetchError,
+		fn: fetchUserFn,
+	} = useFetch(getUser);
+	useEffect(() => {
+		fetchUserFn();
+	}, []);
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <div className="h-10 w-36 bg-gradient-to-r from-[#ff2020] via-[#AA0406] to-[#8F0c11] flex items-center justify-center border-[3px] border-[#FFC363] rounded-full px-6 text-yellow-400 font-bold shadow-md">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="h-6 pr-2"
-          >
-            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-          </svg>
-          <p className="bg-clip-text text-transparent bg-gradient-to-b from-[#FFCD7C] to-[#D4881C]">
-            {isAuthenticated ? "Dashboard" : "Login"}
-          </p>
-        </div>
-      </DropdownMenuTrigger>
+	useEffect(() => {
+		if (userFetchError) {
+			toast({
+				title: "Error",
+				description: "Failed to fetch user data or Token Expired",
+				variant: "destructive",
+			});
+			router.push("/"); // Redirect after logout
+		}
+	}, [userFetchError]);
+	useEffect(() => {
+		setIsAuthenticated(!!userFetchedData); // Set true if user exists
+	}, [userFetchedData]);
 
-      <DropdownMenuContent>
-        {isAuthenticated ? (
-          <>
-            
-            <DropdownMenuItem
-              onClick={() => {
-                router.push("/dashboard");
-              }}
-            >
-              Dashboard
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => {
-                handleLogout();
-              }}
-            >
-              Logout
-            </DropdownMenuItem>
-          </>
-        ) : (
-          <>
-            <DropdownMenuItem
-              onClick={() => {
-                router.push("/auth/signup");
-              }}
-            >
-              SignUp
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => {
-                router.push("/auth/login");
-              }}
-            >
-              Login
-            </DropdownMenuItem>
-          </>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+	// Handle logout
+	const handleLogout = () => {
+		logout(); // Add your logout functionality here
+		setIsAuthenticated(false); // Update the state after logout
+		router.push("/"); // Redirect after logout
+	};
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<div className="h-10 w-36 bg-gradient-to-r from-[#ff2020] via-[#AA0406] to-[#8F0c11] flex items-center justify-center border-[3px] border-[#FFC363] rounded-full px-6 text-yellow-400 font-bold shadow-md">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						className="h-6 pr-2"
+					>
+						<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+					</svg>
+					<p className="bg-clip-text text-transparent bg-gradient-to-b from-[#FFCD7C] to-[#D4881C]">
+						{isAuthenticated ? "Dashboard" : "Login"}
+					</p>
+				</div>
+			</DropdownMenuTrigger>
+
+			<DropdownMenuContent>
+				{isAuthenticated ? (
+					<>
+						<DropdownMenuItem
+							onClick={() => {
+								router.push("/dashboard");
+							}}
+						>
+							Dashboard
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => {
+								handleLogout();
+							}}
+						>
+							Logout
+						</DropdownMenuItem>
+					</>
+				) : (
+					<>
+						<DropdownMenuItem
+							onClick={() => {
+								router.push("/auth/signup");
+							}}
+						>
+							SignUp
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => {
+								router.push("/auth/login");
+							}}
+						>
+							Login
+						</DropdownMenuItem>
+					</>
+				)}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 };
 
 export default UserDropdown;
