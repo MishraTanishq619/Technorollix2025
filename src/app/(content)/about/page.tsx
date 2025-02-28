87d68cde8b642ae3c523fa22992c053106c2abe7
Satendra Kumar <satendrakm27@gmail.com>
2025-02-28 05:36:09 +0530

    events cards completed

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className=" w-full">
       <div className="absolute top-0 left-0 w-full h-full pointer-events-none -z-10">
         <img
-          src="/background.svg"
+          src="/aboutsectionbg.svg"
           className="w-full h-auto opacity-150"
           alt="Scrolling Background"
         />
--- src/components/sub-component/event-intro.tsx
+++ src/components/sub-component/event-intro.tsx
@@ -44,7 +44,7 @@ const EventIntro: React.FC<EventIntroProps> = ({
         {/* Main Content (Ensuring it's above the background) */}
 
         <div className="p-4 pt-48  rounded-lg shadow-lg bg-transparent relative z-10 flex flex-col items-center text-white">
-          <CardwithImage imageUrl={imageUrl} title={title} scale={110} />
+          <Image src={imageUrl} width={200} height={250} alt={"gamefusion"} className="w-full h-auto rounded-xl" />
           <div className="flex mt-10 justify-center text-center text-transparent bg-clip-text bg-gradient-to-b from-[#FFAE3D] via-[#FFD188] to-[#A6660D] text-4xl font-normal font-['Inder'] [text-shadow:_0px_4px_10px_rgb(0_0_0_/_0.78)]">
             Registrations
             <div className="text-center px-4 text-white text-4xl font-normal font-['Inder'] [text-shadow:_0px_4px_10px_rgb(0_0_0_/_0.78)]">
