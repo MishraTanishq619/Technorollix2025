5f3df78d3bc77e43186fc9f82bfa2086eb6233a1
Satendra Kumar <satendrakm27@gmail.com>
2025-03-07 16:40:26 +0530

    sponsors logo chanaged

--- src/components/sponser-marquee.tsx
+++ src/components/sponser-marquee.tsx
@@ -1,30 +1,48 @@
-import { motion } from 'framer-motion';
-import Image from 'next/image';
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const SponsorMarquee: React.FC = () => {
   return (
     <div className="w-full overflow-hidden">
       <motion.div
-        className="flex space-x-48"
-        animate={{ x: ['100%', '-100%'] }}
+        className="flex items-center space-x-12 md:space-x-48"
+        animate={{ x: ["100%", "-100%"] }}
         transition={{
           x: {
             repeat: Infinity,
-            repeatType: 'loop',
+            repeatType: "loop",
             duration: 30,
-            ease: 'linear',
+            ease: "linear",
           },
         }}
         whileHover={{ transition: { x: { duration: 0 } } }}
       >
-        <div className="h-64">
-          <Image src="/jindal-new-logo.png" alt="Sponsor 1" width={300} height={100} objectFit="contain" />
+        <div className="h-40 md:h-64 flex-shrink-0">
+          <Image
+            src="/logo_white.svg"
+            alt="Sponsor 1"
+            width={200}
+            height={66}
+            objectFit="contain"
+          />
         </div>
-        <div className="h-64">
-          <Image src="/jindal-new-logo.png" alt="Sponsor 2" width={300} height={100} objectFit="contain" />
+        <div className="h-40 md:h-64 flex-shrink-0">
+          <Image
+            src="/logo_white.svg"
+            alt="Sponsor 2"
+            width={200}
+            height={66}
+            objectFit="contain"
+          />
         </div>
-        <div className="h-64">
-          <Image src="/jindal-new-logo.png" alt="Sponsor 3" width={300} height={100} objectFit="contain" />
+        <div className="h-40 md:h-64 flex-shrink-0">
+          <Image
+            src="/logo_white.svg"
+            alt="Sponsor 3"
+            width={200}
+            height={66}
+            objectFit="contain"
+          />
         </div>
       </motion.div>
     </div>
