9ac29d28a429f9ee61195f8e83e926b7dd8aaba9
NikhilPatel003 <ntp170405@gmail.com>
2025-03-13 22:12:03 +0530

    modified codigo page

--- src/app/(content)/events/codigo/page.tsx
+++ src/app/(content)/events/codigo/page.tsx
@@ -8,8 +8,9 @@ import Link from "next/link";
 
 const Page = () => {
   const managers = [
-    { imageUrl: "/dallE.png", name: "Ritu Singh", contact: 8839463899 },
-    { imageUrl: "/dallE.png", name: "Sangana Sahu", contact: 9876543210 },
+    { imageUrl: "/managers/codigo/divakar.jpg", name: "Divakar Yadav", contact: 9691300315 },
+    { imageUrl: "/managers/codigo/ritu.jpg", name: "Ritu Singh", contact: 8839463899 },
+    { imageUrl: "/managers/codigo/akash.jpg", name: "Akash Kumar ", contact: 7985757151 },
   ];
 
   return (
@@ -77,7 +78,7 @@ const Page = () => {
         <div className="px-4 text-white mb-32">
           <div className="mb-20">
             <h2 className="text-transparent text-center bg-clip-text bg-gradient-to-b from-[#FFAE3D] via-[#FFD188] to-[#A6660D] text-3xl sm:text-4xl md:text-5xl font-medium font-['Poppins'] uppercase tracking-[3.75px] mb-4 md:mb-20">
-              Event 1: Code-Quest
+              Event 1: Code Challenge
             </h2>
             <div className="bg-[#33010140] rounded-lg pb-4 shadow-lg max-w-5xl mx-auto ">
               <div className=" max-w-5xl mx-auto md:mb-10">
@@ -103,9 +104,10 @@ const Page = () => {
               <div className="max-w-5xl mx-auto md:mb-10">
                 <h3 className="text-xl md:text-2xl lg:text-3xl md:mb-2 font-semibold text-green-600">
                   Round 2 (Screening Round II):{" "}
-                  <span className="text-red-500">Quiz (Google Form)</span>
+                  <span className="text-red-500">MCQs/Fill in the blanks</span>
                 </h3>
                 <ul className="list-disc pl-6 text-gray-300 md:text-lg lg:text-xl">
+                  <li>MCQs + Fill in the blanks type </li>
                   <li>Basic Maths</li>
                   <li>Find Errors in the Code</li>
                   <li>Find Suitable Codes</li>
@@ -116,46 +118,35 @@ const Page = () => {
                 <h3 className="text-xl md:text-2xl lg:text-3xl md:mb-2 font-semibold text-green-600">
                   Round 3 (Final Round):{" "}
                   <span className="text-red-500">
-                    Technical Challenge (HackerRank)
+                    Technical Challenge
                   </span>
                 </h3>
                 <ul className="list-disc pl-6 text-gray-300 md:text-lg lg:text-xl">
                   <li>Maths</li>
                   <li>Patterns</li>
                   <li>Strings</li>
-                  <li>Basic Programming</li>
+                  <li>Real Time Problems</li>
+                  <li>Intermediate Programming</li>
                 </ul>
               </div>
             </div>
           </div>
           <div>
             <h2 className="text-transparent text-center bg-clip-text bg-gradient-to-b from-[#FFAE3D] via-[#FFD188] to-[#A6660D] text-3xl sm:text-4xl md:text-5xl font-medium font-['Poppins'] uppercase tracking-[3.75px] mb-4 md:mb-20">
-              Event 2: Code Craft
+              Event 2: Design Master
             </h2>
             <div className="bg-[#33010140] rounded-lg shadow-lg pb-2 max-w-5xl mx-auto ">
               <div className="max-w-5xl mx-auto  md:mb-10">
                 <h3 className="text-xl md:text-2xl lg:text-3xl md:mb-2 font-semibold text-green-600">
-                  Round 1 (Screening Round):{" "}
-                  <span className="text-red-500">Pattern Storming</span>
+                  Round 1 (Final Round):{" "}
+                  <span className="text-red-500">UX-Design</span>
                 </h3>
                 <ul className="list-disc pl-6 text-gray-300 md:text-lg lg:text-xl">
-                  <li>Code Different Pattern</li>
+                  <li>Theme will provided at the Spot</li>
                 </ul>
               </div>
 
-              <div className="max-w-5xl mx-auto md:mb-10">
-                <h3 className="text-xl md:text-2xl lg:text-3xl md:mb-2 font-semibold text-green-600">
-                  Round 2 (Final Round):{" "}
-                  <span className="text-red-500">
-                    Real Time Challenge (HackerRank)
-                  </span>
-                </h3>
-                <ul className="list-disc pl-6 text-gray-300 md:text-lg lg:text-xl">
-                  <li>
-                    Real Time Problems will be given to find the solutions
-                  </li>
-                </ul>
-              </div>
+              
             </div>
           </div>
         </div>
@@ -167,7 +158,7 @@ const Page = () => {
 
           <div className="bg-[#33010140] rounded-lg shadow-lg pb-2 max-w-5xl mx-auto md:mb-20">
             <h3 className="text-xl md:text-2xl lg:text-3xl md:mb-4 font-semibold text-green-600">
-              Event 1 : Code-Quest
+              Event 1 : Code Challenge
             </h3>
 
             <h4 className="text-lg md:text-xl lg:text-2xl font-semibold text-red-500">
@@ -202,34 +193,26 @@ const Page = () => {
 
           <div className="bg-[#33010140] pb-2 rounded-lg shadow-lg max-w-5xl mx-auto md:mb-10">
             <h3 className="text-xl md:text-2xl lg:text-3xl md:mb-4 font-semibold text-green-600">
-              Event 2 : Code Craft
+              Event 2 : Design Master
             </h3>
 
             <h4 className="text-lg md:text-xl lg:text-2xl font-semibold text-red-500">
-              Round 1: Pattern Storming
+              Round 1: 
             </h4>
             <p className="text-gray-300 md:text-lg lg:text-xl">
-              Create patterns.{" "}
-              <span className="text-blue-500 font-semibold">
-                Minimum requirement: 3 patterns
-              </span>
-              . Time limit - 20 min.
+            Themes of the Event will be provided. Judges will decide.
+              
             </p>
 
-            <h4 className="text-lg md:text-xl lg:text-2xl font-semibold text-red-500 mt-4">
-              Round 2: Real-Time Challenge (HackerRank)
-            </h4>
-            <p className="text-gray-300 md:text-lg lg:text-xl">
-              Given 5 questions, solve as many as possible within 30 min.
-            </p>
+            
           </div>
         </div>
         <section className="mb-32">
-          <h2 className="text-5xl text-transparent bg-clip-text bg-gradient-to-b from-[#FFAE3D] via-[#FFD188] to-[#A6660D] font-medium text-center mb-12">
+          <h2 className="text-3xl sm:text-4xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-b from-[#FFAE3D] via-[#FFD188] to-[#A6660D] font-medium text-center mb-12">
             Winner & Judgement Criteria:
           </h2>
           <div className="bg-[#33010140] p-6 rounded-lg shadow-lg max-w-5xl mx-auto">
-            <ul className="list-disc pl-5 text-2xl space-y-2 text-gray-300">
+            <ul className="list-disc pl-5 text-xl lg:text-2xl space-y-2 text-gray-300">
               <li>Maximum Questions Solved</li>
               <li>Maximum Test Cases Passed</li>
               <li>Submission Time</li>
--- src/app/(content)/events/kalakriti/page.tsx
+++ src/app/(content)/events/kalakriti/page.tsx
@@ -11,43 +11,67 @@ import { FaFlipboard } from "react-icons/fa6";
 import { TbBottleFilled } from "react-icons/tb";
 import RoundSection from "@/components/sub-component/RoundSection";
 
-
-
-
 const KalaKriti = () => {
   const managers = [
-    { imageUrl: "/managers/Kalakriti/Aditi.jpg", name: "Aditi", contact: 8839171099 },
-    { imageUrl: "/managers/Kalakriti/AnkitK.jpeg", name: "Ankit", contact: 8839171099 },
-    { imageUrl: "/managers/Kalakriti/Bhavya.jpg", name: "Bhavya", contact: 8839171099 },
-    { imageUrl: "/managers/Kalakriti/Mohini.jpg", name: "Mohini", contact: 8839171099 },
-    { imageUrl: "/managers/Kalakriti/pranav.webp", name: "Pranav", contact: 8839171099 },
-  ];
-  const rounds=[
     {
-        title: "Pradarshini (Exhibition)",
-        description: "▣ Theme: All Kinds of Art and Collection. ▣ About: Imagine what life would have been without colors, art and aesthetics—boring, right? And what Techno would be without an art exhibition? But don't worry, we would not give you a millisecond of it. So, to dazzle your eyes we have Pradarshini, an art exhibition by Kalakriti, to quench the thirst of your imagination and take you on a bewildering journey worth remembering. Get yourself a pair of sunglasses and come engulf yourself in art and aesthetics. ▣ Venue: OPJU Library\n ▣ Rules:\n1. All participants must register before the event.\n2. Submit your Artistic Piece, Art or Craft 2-3 days before the event.\n3. Last-minute changes, if necessary, can be made.\n ▣ Rounds: 1 Round\n ▣ Judgement Criteria: No Grading"
-      },
-      {
-        title: "Drishyaam (Decoration)",
-        description: "▣ Theme: Festivals of India\n▣ About: India is a land of diverse cultures, reflected in its vibrant array of festivals, with major celebrations including Holi, Diwali, Ganesh Chaturthi, Durga Puja, Onam, and Maha Shivratri; each marked by unique rituals, food, and festivities across the country—showcasing India's rich heritage and unity in diversity.\n▣ Venue: OPJU Campus\n▣ Rules:\n1. The competition theme is restricted to “Festivals of India.”\n2. Groups must have a minimum of 4 and a maximum of 6 members; boys’ participation is mandatory.\n3. There will be only one round.\n4. All materials are provided by the organizers.\n5. Participants must decorate their respective sets on or before the 1st day of Technorollix.\n6. Cash prizes will be awarded to the top three positions along with Certificates of Achievement. All participants receive a Certificate of Participation.\n7. Adhere strictly to timings.\n8. The decision of the jury is final.\n9. Last-minute changes, if necessary, can be made.\n▣ Rounds: 1 Round\n▣ Judgement Criteria: Judges’ marks"
-      },
-      {
-        title: "Photorollix",
-        description: "▣ Theme: Raigarh, JSP, OPJU Campus, and Nature\n▣ About: Photorollix is a recreational event where participants showcase their 'third eye' skills. As they say, the picture you take is the imagination you want to create with reality. It will be a hall of fame featuring beautiful moments captured by photographers—allowing you to relive amazing memories.\n▣ Venue: Online\n▣ Rules:\n1. The event will take place in 2 rounds over two days.\n2. 1st round before 1st Mar and 2nd round before 2nd Feb 2023.\n3. Each participant submits one photo via Google Form at the end of the day, which will be framed in the gallery.\n4. Last-minute changes, if necessary, can be made.\n▣ Rounds: 2 Rounds\n▣ Judgement Criteria: Judges"
-      },
-      {
-        title: "Rangoli",
-        description: "▣ Theme: On the Spot\n▣ Venue: Library Lawn & opposite Chemistry Lab.\n▣ Rounds: 1 Round\n▣ Rules and Regulations:\n1. Groups must have 4 to 6 members; boys’ participation is mandatory.\n2. Only one round of competition.\n3. Participants bring their own materials (except colors, provided by organizers).\n4. Participants have 2 hours to create the rangoli. No extra time.\n5. Prizes for top three positions along with certificates; all receive a participation certificate.\n6. Strict adherence to timings.\n▣ Judgement Criteria: Judges’ marks"
-      },
-      {
-        title: "Colour Painting / Sketching",
-        description: "▣ Theme: On the Spot\n▣ About: In order to create, we draw from our inner well—our artistic reservoir. If we neglect to nurture it, our creativity may stagnate. As artists, we must replenish our creative resources. Let your brush or pencil speak on canvas.\n▣ Venue: Library\n▣ Rules:\n1. The theme is restricted to “On the Spot.”\n2. Only one round.\n3. Participants must bring their own materials; paper/canvas provided by organizers.\n4. 2 hours allotted—no extra time.\n5. Prizes for the top three positions along with Certificates of Achievement; all receive a participation certificate.\n6. Strict adherence to timings.\n7. Jury’s decision is final.\n8. Last-minute changes, if necessary, can be made.\n▣ Rounds: 1 Round"
-      },
-      {
-        title: "Bottle Painting",
-        description: "Bottle Painting transforms the ordinary into art. Let your creativity flow as you turn simple bottles into vibrant masterpieces, blending colors with imagination!"
-      }   
-  ]
+      imageUrl: "/managers/Kalakriti/nupur.jpg",
+      name: "Nupur",
+      contact: 9301254290,
+    },
+    {
+      imageUrl: "/managers/Kalakriti/AnkitK.jpeg",
+      name: "Ankit",
+      contact: 8839171099,
+    },
+    {
+      imageUrl: "/managers/Kalakriti/Bhavya.jpg",
+      name: "Bhavya",
+      contact: 8839171099,
+    },
+    {
+      imageUrl: "/managers/Kalakriti/Aditi.jpg",
+      name: "Aditi",
+      contact: 8839171099,
+    },
+    {
+      imageUrl: "/managers/Kalakriti/pranav.webp",
+      name: "Pranav",
+      contact: 8839171099,
+    },
+  ];
+
+  const rounds = [
+    {
+      title: "Pradarshini (Exhibition)",
+      description:
+        "▣ Theme: All Kinds of Art and Collection. ▣ About: Imagine what life would have been without colors, art and aesthetics—boring, right? And what Techno would be without an art exhibition? But don't worry, we would not give you a millisecond of it. So, to dazzle your eyes we have Pradarshini, an art exhibition by Kalakriti, to quench the thirst of your imagination and take you on a bewildering journey worth remembering. Get yourself a pair of sunglasses and come engulf yourself in art and aesthetics. ▣ Venue: OPJU Library\n ▣ Rules:\n1. All participants must register before the event.\n2. Submit your Artistic Piece, Art or Craft 2-3 days before the event.\n3. Last-minute changes, if necessary, can be made.\n ▣ Rounds: 1 Round\n ▣ Judgement Criteria: No Grading",
+    },
+    {
+      title: "Drishyaam (Decoration)",
+      description:
+        "▣ Theme: Festivals of India\n▣ About: India is a land of diverse cultures, reflected in its vibrant array of festivals, with major celebrations including Holi, Diwali, Ganesh Chaturthi, Durga Puja, Onam, and Maha Shivratri; each marked by unique rituals, food, and festivities across the country—showcasing India's rich heritage and unity in diversity.\n▣ Venue: OPJU Campus\n▣ Rules:\n1. The competition theme is restricted to “Festivals of India.”\n2. Groups must have a minimum of 4 and a maximum of 6 members; boys’ participation is mandatory.\n3. There will be only one round.\n4. All materials are provided by the organizers.\n5. Participants must decorate their respective sets on or before the 1st day of Technorollix.\n6. Cash prizes will be awarded to the top three positions along with Certificates of Achievement. All participants receive a Certificate of Participation.\n7. Adhere strictly to timings.\n8. The decision of the jury is final.\n9. Last-minute changes, if necessary, can be made.\n▣ Rounds: 1 Round\n▣ Judgement Criteria: Judges’ marks",
+    },
+    {
+      title: "Photorollix",
+      description:
+        "▣ Theme: Raigarh, JSP, OPJU Campus, and Nature\n▣ About: Photorollix is a recreational event where participants showcase their 'third eye' skills. As they say, the picture you take is the imagination you want to create with reality. It will be a hall of fame featuring beautiful moments captured by photographers—allowing you to relive amazing memories.\n▣ Venue: Online\n▣ Rules:\n1. The event will take place in 2 rounds over two days.\n2. 1st round before 1st Mar and 2nd round before 2nd Feb 2023.\n3. Each participant submits one photo via Google Form at the end of the day, which will be framed in the gallery.\n4. Last-minute changes, if necessary, can be made.\n▣ Rounds: 2 Rounds\n▣ Judgement Criteria: Judges",
+    },
+    {
+      title: "Rangoli",
+      description:
+        "▣ Theme: On the Spot\n▣ Venue: Library Lawn & opposite Chemistry Lab.\n▣ Rounds: 1 Round\n▣ Rules and Regulations:\n1. Groups must have 4 to 6 members; boys’ participation is mandatory.\n2. Only one round of competition.\n3. Participants bring their own materials (except colors, provided by organizers).\n4. Participants have 2 hours to create the rangoli. No extra time.\n5. Prizes for top three positions along with certificates; all receive a participation certificate.\n6. Strict adherence to timings.\n▣ Judgement Criteria: Judges’ marks",
+    },
+    {
+      title: "Colour Painting / Sketching",
+      description:
+        "▣ Theme: On the Spot\n▣ About: In order to create, we draw from our inner well—our artistic reservoir. If we neglect to nurture it, our creativity may stagnate. As artists, we must replenish our creative resources. Let your brush or pencil speak on canvas.\n▣ Venue: Library\n▣ Rules:\n1. The theme is restricted to “On the Spot.”\n2. Only one round.\n3. Participants must bring their own materials; paper/canvas provided by organizers.\n4. 2 hours allotted—no extra time.\n5. Prizes for the top three positions along with Certificates of Achievement; all receive a participation certificate.\n6. Strict adherence to timings.\n7. Jury’s decision is final.\n8. Last-minute changes, if necessary, can be made.\n▣ Rounds: 1 Round",
+    },
+    {
+      title: "Bottle Painting",
+      description:
+        "Bottle Painting transforms the ordinary into art. Let your creativity flow as you turn simple bottles into vibrant masterpieces, blending colors with imagination!",
+    },
+  ];
 
   return (
     <div className="relative space-y-10 px-4 py-8">
@@ -80,7 +104,11 @@ const KalaKriti = () => {
           Sub-events
         </h2>
         <p className="text-white text-2xl font-normal font-['Inter'] tracking-[3.75px]">
-          KalaKriti brings you a vibrant celebration of art and creativity! Explore the exciting sub-events—Pradarshini (Exhibition), Drishyaam (Decoration), Photorollix, Rangoli, Colour Painting, and Bottle Painting. Unleash your imagination, showcase your artistic skills, and be part of this colorful extravaganza!
+          KalaKriti brings you a vibrant celebration of art and creativity!
+          Explore the exciting sub-events—Pradarshini (Exhibition), Drishyaam
+          (Decoration), Photorollix, Rangoli, Colour Painting, and Bottle
+          Painting. Unleash your imagination, showcase your artistic skills, and
+          be part of this colorful extravaganza!
         </p>
       </section>
 
@@ -118,6 +146,9 @@ const KalaKriti = () => {
             description="Bottle Painting transforms the ordinary into art. Let your creativity flow as you turn simple bottles into vibrant masterpieces, blending colors with imagination!"
           />
         </div>
+      </section>
+      <section>
+        
       </section>
 
       <section className="px-4">
